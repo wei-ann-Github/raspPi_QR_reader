348b9e6e3230cc62d6914c58d6c0f5fdab5d5cbb
wei-ann-github <wei-ann.lim@outlook.com>
2018-10-02 09:46:33 +0800

    minor edit

--- js version/instascan/js/utils.js
+++ js version/instascan/js/utils.js
@@ -1,15 +1,22 @@
 /* Global variables */
 var msg
 
+/* content processing */
+function split_word(content) {
+	var contents = content.trim().split(" ");
+	var index_last = contents.length - 1;
+	console.log(contents[index_last].lower().trim());
+	return contents[index_last].lower().trim()
+}
+
 /* Functions for querying the DB */
 function askServer(id) {
-	if (id == "null")  // This is for getting the id when the Enter button is pressed.
+	/*if (id == "null")  // This is for getting the id when the Enter button is pressed.
 		id = document.getElementById('enterSearchString').value
 	if (id.length == 0)
 		return
-	document.getElementById('enterSearchString').value = ""
+	document.getElementById('enterSearchString').value = ""*/
 	var dataToSend = "?id=" + id;
-	console.log(dataToSend);
 	req.open("GET", "db.php" + dataToSend, true);
 	req.onreadystatechange = handleServerResponse;
 	req.send();
@@ -42,7 +49,13 @@ function startScan() {
 	// The scanner starts
 	let scanner = new Instascan.Scanner({ video: document.getElementById('preview') });
 	scanner.addListener('scan', function (content) {
+		console.log("scanned: " + content)
 		openModal()
+		var contents = content.trim().split(" ")
+		var len_contents = contents.length
+		console.log(contents)
+		console.log("in scanner " + contents[len_contents-1])
+		content = contents[len_contents-1]
 		askServer(content);
 	});
 	Instascan.Camera.getCameras().then(function (cameras) {
@@ -55,4 +68,16 @@ function startScan() {
 		console.error(e);
 	});
 	return
+}
+
+/* Time conversions */
+function getLocalTime_from_unix(unix) {
+	var date = new Date(unix*1000);  // X1000 to make it miliseconds
+	// Hours part from the timestamp
+	var hours = date.getHours();
+	// Minutes part from the timestamp
+	var minutes = "0" + date.getMinutes();
+	// Seconds part from the timestamp
+	var seconds = "0" + date.getSeconds();
+	return hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
 }
